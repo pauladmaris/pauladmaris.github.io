f78c57af4f7c3170676394aab72120886d577ad9
pauladmaris <pauladmaris@yahoo.com>
2022-09-26 18:19:52 +0300

    add scroll transition

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active-reveal");
+    } else {
+      reveals[i].classList.remove("active-reveal");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 /* EXPERIENCE.COMPONENT Slideshow */
 var slideIndex = 1;
 var slideIndex2 = 1;
